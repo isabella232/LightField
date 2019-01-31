6a8420341014e8a4a39086cab46828414dade877
Ice Karma <icekarma@zive.ca>
2019-01-31 11:15:21 -0800

    New header file for generating a precompiled header.

---
+++ src/pch.h
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdlib>
+
+#include <future>
+#include <vector>
+
+#include <QApplication>
+#include <QDataStream>
+#include <QDebug>
+#include <QFile>
+#include <QFileOpenEvent>
+#include <QFileSystemModel>
+#include <QGridLayout>
+#include <QListView>
+#include <QMainWindow>
+#include <QMouseEvent>
+#include <QOpenGLBuffer>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QProcess>
+#include <QString>
+#include <QSurfaceFormat>
+#include <QtDebug>
+#include <QThread>
+#include <QtOpenGL>
+#include <QtOpenGL/QtOpenGL>
+#include <QVector3D>
