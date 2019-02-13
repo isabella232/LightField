5a3a060b2d896adaca514140767f57da4bef2dc1
Ice Karma <icekarma@zive.ca>
2019-02-13 09:28:12 -0800

    Fix a release-build warning.

--- src/debug.h
+++ src/debug.h
@@ -1,11 +1,16 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
+#if defined _DEBUG
 template<typename... Args>
 inline void debug( char const* fmt, Args... args ) {
-#if defined _DEBUG
     fprintf( stderr, fmt, args... );
-#endif // _DEBUG
 }
+#else
+template<typename... Args>
+inline void debug( char const*, Args... ) {
+    /*empty*/
+}
+#endif // _DEBUG
 
 #endif // __DEBUG_H__
