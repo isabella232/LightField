a0d4b723ea7a593fdad87ca651750a699f880ad8
Ice Karma <icekarma@zive.ca>
2019-02-13 10:37:42 -0800

    Add new function getFileBaseName.

--- src/strings.h
+++ src/strings.h
@@ -1,6 +1,10 @@
 #ifndef __STRINGS_H__
 #define __STRINGS_H__
 
+inline QString getFileBaseName( QString const& fileName ) {
+    return fileName.mid( fileName.lastIndexOf( QChar( '/' ) ) + 1 );
+}
+
 char const* ToString( QProcess::ProcessError value );
 char const* ToString( QProcess::ProcessState value );
 char const* ToString( QProcess::ExitStatus   value );
@@ -8,8 +12,5 @@ char const* ToString( QProcess::ExitStatus   value );
 #if defined __SHEPHERD_H__
 char const* ToString( PendingCommand         value );
 #endif // __SHEPHERD_H__
-
-// template:
-//char const* ToString( typename value );
 
 #endif // __STRINGS_H__
