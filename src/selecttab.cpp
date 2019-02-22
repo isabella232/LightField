076c20af740c79d69c3baa4ce02d1ccaf64c444a
Ice Karma <icekarma@zive.ca>
2019-02-21 20:17:43 -0800

    Adjust formatting, fix unit conversion error.

--- src/selecttab.cpp
+++ src/selecttab.cpp
@@ -154,11 +154,11 @@ void SelectTab::loader_gotMesh( Mesh* m ) {
     );
 
     {
-        auto sizeXstring = GroupDigits( QString( "%1" ).arg( sizeX,                 0, 'f', 2 ), ' ' );
-        auto sizeYstring = GroupDigits( QString( "%1" ).arg( sizeY,                 0, 'f', 2 ), ' ' );
-        auto sizeZstring = GroupDigits( QString( "%1" ).arg( sizeZ,                 0, 'f', 2 ), ' ' );
-        auto volume      = GroupDigits( QString( "%1" ).arg( sizeX * sizeY * sizeZ, 0, 'f', 2 ), ' ' );
-        _dimensionsLabel->setText( QString( "%1×%2×%3 mm  •  %4 ml" ).arg( sizeXstring ).arg( sizeYstring ).arg( sizeZstring ).arg( volume ) );
+        auto sizeXstring = GroupDigits( QString( "%1" ).arg( sizeX,                          0, 'f', 2 ), ' ' );
+        auto sizeYstring = GroupDigits( QString( "%1" ).arg( sizeY,                          0, 'f', 2 ), ' ' );
+        auto sizeZstring = GroupDigits( QString( "%1" ).arg( sizeZ,                          0, 'f', 2 ), ' ' );
+        auto volume      = GroupDigits( QString( "%1" ).arg( sizeX * sizeY * sizeZ / 1000.0, 0, 'f', 2 ), ' ' );
+        _dimensionsLabel->setText( QString( "%1 mm × %2 mm × %3 mm  •  %4 ml" ).arg( sizeXstring ).arg( sizeYstring ).arg( sizeZstring ).arg( volume ) );
     }
 
     if ( ( sizeX > PrinterMaximumX ) || ( sizeY > PrinterMaximumY ) || ( sizeZ > PrinterMaximumZ ) ) {
