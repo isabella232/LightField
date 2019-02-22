9fd165719ed89ff5875cea4895fa9dfaa9e9ac31
Ice Karma <icekarma@zive.ca>
2019-02-22 10:58:03 -0800

    Helps if I remember to add new files.

---
+++ src/welcometab.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+
+#include "welcometab.h"
+
+WelcomeTab::WelcomeTab( QWidget* parent ): QWidget( parent ) {
+    _logoLabel->setPixmap( QPixmap( QString( ":lf/dark-logo.png" ) ) );
+    _versionLabel->setText( QString( "1.0.0" ) );
+
+    _layout->addStretch( );
+    _layout->addWidget( _logoLabel );
+    _layout->addStretch( );
+    _layout->addWidget( _versionLabel );
+    _layout->addStretch( );
+
+    setLayout( _layout );
+}
+
+WelcomeTab::~WelcomeTab( ) {
+    /*empty*/
+}
+
+void WelcomeTab::setPrintJob( PrintJob* printJob ) {
+    debug( "+ WelcomeTab::setPrintJob: printJob %p\n", printJob );
+    _printJob = printJob;
+}
+
+void WelcomeTab::setShepherd( Shepherd* newShepherd ) {
+    _shepherd = newShepherd;
+}
