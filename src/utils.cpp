da422e18af99ea0dfb27159aa009cd0ff3845ffb
Ice Karma <icekarma@zive.ca>
2019-02-22 18:42:59 -0800

    Fix USB autodetection to work according to reality.

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 
+#include <pwd.h>
+
 #include "utils.h"
 
 QVBoxLayout* WrapWidgetInVBox( QWidget* widget, Qt::AlignmentFlag const alignment ) {
@@ -22,4 +24,37 @@ QFont ModifyFont( QFont const& font_, float const pointSizeF, QFont::Weight cons
     font.setPointSizeF( pointSizeF );
     font.setWeight( weight );
     return font;
+}
+
+QString GetUserName( ) {
+    struct passwd pwd;
+    char* buf = new char[16384];
+    struct passwd* result;
+
+    if ( 0 != ::getpwuid_r( ::getuid( ), &pwd, buf, 16384, &result ) ) {
+        debug( "@@@ + SelectTab::_lookForUsbStick: getpwuid_r failed?!\n" );
+        delete[] buf;
+        return QString( );
+    }
+
+    QString userName { pwd.pw_name };
+    delete[] buf;
+    return userName;
+}
+
+QString GetFirstDirectoryIn( QString const& directory ) {
+    auto dir = new QDir( directory );
+    dir->setFilter( QDir::Dirs );
+
+    QString dirname;
+    for ( auto name : dir->entryList( ) ) {
+        if ( ( name == "." ) || ( name == ".." ) ) {
+            continue;
+        }
+
+        dirname = name;
+        break;
+    }
+
+    return dirname;
 }
