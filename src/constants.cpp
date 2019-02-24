82366ab7d548768db9eb3bef83feeb8e45585598
Ice Karma <icekarma@zive.ca>
2019-02-24 12:25:40 -0800

    Reduce height of MaximalRightHandPaneSize to compensate for increasing the height of the title bar, and remove unused constant UsbStickPath.

--- src/constants.cpp
+++ src/constants.cpp
@@ -1,7 +1,6 @@
 #include "pch.h"
 
 QSize   const MainWindowSize           {  800, 480 };
-QSize   const MaximalRightHandPaneSize {  564, 451 };
+QSize   const MaximalRightHandPaneSize {  564, 424 };
 QSize   const PngDisplayWindowSize     { 1280, 800 };
 QString const StlModelLibraryPath      { "/home/lumen/Volumetric/model-library" };
-QString const UsbStickPath             { "/media/lumen" };
--- src/constants.h
+++ src/constants.h
@@ -5,7 +5,6 @@ QSize   extern const MainWindowSize;
 QSize   extern const MaximalRightHandPaneSize;
 QSize   extern const PngDisplayWindowSize;
 QString extern const StlModelLibraryPath;
-QString extern const UsbStickPath;
 
 double         const PrinterMaximumX          = 64.0;
 double         const PrinterMaximumY          = 40.0;
