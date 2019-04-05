21a0eaf07a6b53200732ef6307ac7b22f8ebec7e
Ice Karma <icekarma@zive.ca>
2019-04-05 11:33:03 -0700

    [cleanup] Avoid risk of getting the wrong function by prefixing libc functions with the global scope override operator.

--- src/debug.cpp
+++ src/debug.cpp
@@ -22,30 +22,28 @@ namespace {
 
 DebugManager::DebugManager( ) {
     ::unlink( DebugLogPaths[5] );
-    ::rename( DebugLogPaths[4], DebugLogPaths[5] );
-    ::rename( DebugLogPaths[3], DebugLogPaths[4] );
-    ::rename( DebugLogPaths[2], DebugLogPaths[3] );
-    ::rename( DebugLogPaths[1], DebugLogPaths[2] );
-    ::rename( DebugLogPaths[0], DebugLogPaths[1] );
+    for ( int n = 5; n > 0; --n ) {
+        ::rename( DebugLogPaths[n - 1], DebugLogPaths[n] );
+    }
 
     DebugLog = ::fopen( DebugLogPaths[0], "wtx" );
     if ( !DebugLog ) {
         error_t err = errno;
-        fprintf( stderr, "failed to open log file '%s': %s [%d]", DebugLogPaths[0], strerror( err ), err );
+        ::fprintf( stderr, "failed to open log file '%s': %s [%d]", DebugLogPaths[0], strerror( err ), err );
         DebugLog = stderr;
     } else {
         // save the original stderr
-        int fd = dup( 2 );
+        int fd = ::dup( 2 );
 
         // redirect stderr to the debug log
-        dup2( ::fileno( DebugLog ), 2 );
+        ::dup2( ::fileno( DebugLog ), 2 );
 
         // get a FILE* for the original stderr
-        OriginalStderr = fdopen( fd, "wt" );
+        OriginalStderr = ::fdopen( fd, "wt" );
 
         // disable buffering on both FILE*:s
-        setvbuf( DebugLog,       nullptr, _IONBF, 0 );
-        setvbuf( OriginalStderr, nullptr, _IONBF, 0 );
+        ::setvbuf( DebugLog,       nullptr, _IONBF, 0 );
+        ::setvbuf( OriginalStderr, nullptr, _IONBF, 0 );
     }
 }
 
@@ -54,8 +52,8 @@ DebugManager::~DebugManager( ) {
 }
 
 void debug( char const* str ) {
-    fputs( str, DebugLog       );
-    fputs( str, OriginalStderr );
+    ::fputs( str, DebugLog       );
+    ::fputs( str, OriginalStderr );
 }
 
 #endif // defined _DEBUG
