83eddc9f8f865192a306e5b61d22e2670e3addc0
Ice Karma <icekarma@zive.ca>
2019-04-05 11:33:14 -0700

    Whitespace change.

--- LightFieldSymlinksFix.sh
+++ LightFieldSymlinksFix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 mkdir /var/{cache,lib}/lightfield
 mkdir -p /usr/share/lightfield/libexec
 ln -s /home/lumen/Volumetric/working /var/cache/lightfield/print-jobs
@@ -7,4 +8,3 @@ ln -s /home/lumen/Volumetric/debug /var/log/lightfield
 ln -s /home/lumen/Volumetric/printrun /usr/share/lightfield/libexec/printrun
 ln -s /home/lumen/Volumetric/LightField/stdio-shepherd /usr/share/lightfield/libexec/stdio-shepherd
 chown -hR lumen:lumen /var/{cache,lib,log}/lightfield
-
