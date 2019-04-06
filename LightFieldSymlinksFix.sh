f4ea33dbeb4442043f064223afb3e5055338331c
Ice Karma <icekarma@zive.ca>
2019-04-06 16:46:52 -0700

    Make mkdir, ln, and chown commands verbose.

--- LightFieldSymlinksFix.sh
+++ LightFieldSymlinksFix.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir /var/{cache,lib}/lightfield
-mkdir -p /usr/share/lightfield/libexec
-ln -s /home/lumen/Volumetric/working /var/cache/lightfield/print-jobs
-ln -s /home/lumen/Volumetric/model-library /var/lib/lightfield/model-library
-ln -s /home/lumen/Volumetric/debug /var/log/lightfield
-ln -s /home/lumen/Volumetric/printrun /usr/share/lightfield/libexec/printrun
-ln -s /home/lumen/Volumetric/LightField/stdio-shepherd /usr/share/lightfield/libexec/stdio-shepherd
-chown -hR lumen:lumen /var/{cache,lib,log}/lightfield
+mkdir  -v /var/{cache,lib}/lightfield
+mkdir -pv /usr/share/lightfield/libexec
+ln -sv /home/lumen/Volumetric/working /var/cache/lightfield/print-jobs
+ln -sv /home/lumen/Volumetric/model-library /var/lib/lightfield/model-library
+ln -sv /home/lumen/Volumetric/debug /var/log/lightfield
+ln -sv /home/lumen/Volumetric/printrun /usr/share/lightfield/libexec/printrun
+ln -sv /home/lumen/Volumetric/LightField/stdio-shepherd /usr/share/lightfield/libexec/stdio-shepherd
+chown -chR lumen:lumen /var/{cache,lib,log}/lightfield
