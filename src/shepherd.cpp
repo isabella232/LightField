38645d92f66ac0358c7646521d02d90c157c5fc5
Ice Karma <icekarma@zive.ca>
2019-04-11 10:01:59 -0700

    Renaming.

--- src/shepherd.cpp
+++ src/shepherd.cpp
@@ -360,7 +360,7 @@ void Shepherd::start( ) {
     _processRunner = new ProcessRunner( this );
     QObject::connect( _processRunner, &ProcessRunner::succeeded, this, &Shepherd::processRunner_succeeded );
     QObject::connect( _processRunner, &ProcessRunner::failed,    this, &Shepherd::processRunner_failed    );
-    _processRunner->start( "reset-printer-port", { } );
+    _processRunner->start( "reset-lumen-arduino-port", { } );
 }
 
 void Shepherd::doMoveRelative( float const relativeDistance ) {
