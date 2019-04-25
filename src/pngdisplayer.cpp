c42a2c25c0854c6613c68c6dc4546a565b17f4f5
Ice Karma <icekarma@zive.ca>
2019-04-24 18:01:20 -0700

    Effectuate -x and -y command-line parameters by adjusting not merely window position but size as well, to hide the inappropriate (i.e., not black) desktop wallpaper.

--- src/pngdisplayer.cpp
+++ src/pngdisplayer.cpp
@@ -3,17 +3,57 @@
 #include "pngdisplayer.h"
 
 #include "app.h"
+#include "strings.h"
 #include "utils.h"
 
 PngDisplayer::PngDisplayer( QWidget* parent ): QMainWindow( parent ) {
-    setFixedSize( PngDisplayWindowSize );
+    _label->setAlignment( Qt::AlignCenter );
+
+    QPoint topLeft    { g_settings.pngDisplayWindowPosition };
+    QSize  windowSize { PngDisplayWindowSize                };
+
+    auto offsetX = g_settings.projectorOffset.x( );
+    auto offsetY = g_settings.projectorOffset.y( );
+
+    if ( offsetX < 0 ) {
+        topLeft.setX( topLeft.x( ) + offsetX );
+    }
+    if ( offsetY < 0 ) {
+        topLeft.setY( topLeft.y( ) + offsetY );
+    }
+
+    windowSize += QSize { abs( offsetX ), abs( offsetY ) };
+
+    setFixedSize( windowSize );
     setPalette( ModifyPalette( palette( ), QPalette::Window, Qt::black ) );
     setSizePolicy( QSizePolicy::Fixed, QSizePolicy::Fixed );
     setWindowFlags( windowFlags( ) | ( g_settings.frameless ? Qt::FramelessWindowHint : Qt::BypassWindowManagerHint ) );
-    move( g_settings.pngDisplayWindowPosition + g_settings.projectorOffset );
+    move( topLeft );
 
-    _label->setAlignment( Qt::AlignCenter );
-    setCentralWidget( _label );
+    auto topPadding = new QWidget;
+    topPadding->setContentsMargins( { } );
+    topPadding->setFixedSize( windowSize.width( ), offsetY );
+    topPadding->setSizePolicy( QSizePolicy::Fixed, QSizePolicy::Fixed );
+
+    auto leftPadding = new QWidget;
+    leftPadding->setContentsMargins( { } );
+    leftPadding->setFixedSize( offsetX, windowSize.height( ) );
+    leftPadding->setSizePolicy( QSizePolicy::Fixed, QSizePolicy::Fixed );
+
+    QHBoxLayout* hbox = new QHBoxLayout;
+    hbox->addWidget( leftPadding );
+    hbox->addWidget( _label );
+    hbox->setContentsMargins( { } );
+
+    QVBoxLayout* vbox = new QVBoxLayout;
+    vbox->addWidget( topPadding );
+    vbox->addLayout( hbox );
+    vbox->setContentsMargins( { } );
+
+    QWidget* centralWidget = new QWidget;
+    centralWidget->setLayout( vbox );
+
+    setCentralWidget( centralWidget );
 }
 
 PngDisplayer::~PngDisplayer( ) {
