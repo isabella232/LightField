fd0ac26dceed93f43e5063836179077977715172
Ice Karma <icekarma@zive.ca>
2019-04-29 09:24:03 -0700

    Change printer max Z to 50.00 mm.

--- src/constants.h
+++ src/constants.h
@@ -29,7 +29,7 @@ QChar   extern const FA_FastForward;
 
 double         const PrinterMaximumX          = 64.00;
 double         const PrinterMaximumY          = 40.00;
-double         const PrinterMaximumZ          = 40.00;
+double         const PrinterMaximumZ          = 50.00;
 double         const PrinterRaiseToMaxZHeight = 70.00;
 
 double         const AspectRatio5to3          =  5.0 /  3.0;
