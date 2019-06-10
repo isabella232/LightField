b41e467a2492b600e130029af04d4bec5f70f613
Ice Karma <icekarma@zive.ca>
2019-06-10 14:18:49 -0700

    Add mountmon to disable and uninstall scripts.

--- disable-lightfield.sh
+++ disable-lightfield.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+
 sudo systemctl stop getty@tty1
 sudo systemctl disable getty@tty1
 sudo systemctl set-default graphical.target
-sudo rm -rf /etc/systemd/system/getty*
+sudo rm -rf /etc/systemd/system/getty@tty1*
 sudo systemctl daemon-reload
 sudo rm /usr/share/X11/xorg.conf.d/99-waveshare.conf
 sudo systemctl reboot
-
--- uninstall-lightfield.sh
+++ uninstall-lightfield.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-sudo rm -rf /var/cache/lightfield/ /var/lib/lightfield/ /var/log/lightfield/ /etc/sudoers.d/lumen-lightfield /etc/systemd/system/getty* /home/lumen/.bash_profile /lib/systemd/system/lightfield.service /usr/bin/lf /usr/bin/set-projector-power /usr/share/lightfield/ /usr/share/X11/xorg.conf.d/99-waveshare.conf /lib/udev/rules.d/90-dlpc350.rules
+
+/usr/bin/sudo /bin/rm -rd                        \
+	/etc/sudoers.d/lumen-lightfield              \
+	/etc/systemd/system/getty@tty1*              \
+	/home/lumen/.bash_profile                    \
+	/lib/systemd/system/lightfield.service       \
+	/lib/udev/rules.d/90-dlpc350.rules           \
+	/usr/bin/lf                                  \
+	/usr/bin/mountmon                            \
+	/usr/bin/set-projector-power                 \
+	/usr/share/lightfield/                       \
+	/usr/share/X11/xorg.conf.d/99-waveshare.conf \
+	/var/cache/lightfield/                       \
+	/var/lib/lightfield/                         \
+	/var/log/lightfield/
