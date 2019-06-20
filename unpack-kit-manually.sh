5872e43a52ab2ef1e641f63fe66595d9097357f7
Ice Karma <icekarma@zive.ca>
2019-06-19 17:32:59 -0700

    Add manual unpack script, version-changing patch.

---
+++ unpack-kit-manually.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "${UID}" != "0" ]
+then
+    echo This script must be run as root.
+    exit 1
+fi
+
+UPDATES_DIR=/var/lib/lightfield/software-updates
+
+rm    -v -dr "${UPDATES_DIR}"
+mkdir -v -p  "${UPDATES_DIR}"
+
+for ver in 1.0.0
+do
+	dir="lightfield-debug_${ver}_amd64"
+	updatedir="${UPDATES_DIR}/${dir}"
+	rm    -frv     "${updatedir}"
+	mkdir -v       "${updatedir}"
+	tar   -v -v -C "${updatedir}" -xf ~/"${dir}.kit"
+done
+
+chown -c -R  lumen:lumen "${UPDATES_DIR}"
+
+echo "deb file:/var/lib/lightfield/software-updates/lightfield-debug_1.0.0_amd64 ./" > /etc/apt/sources.list.d/volumetric-lightfield.list
+chown -c lumen:lumen /etc/apt/sources.list.d/volumetric-lightfield.list
+
+cp    -v ~/volumetric-keyring.gpg /etc/apt/trusted.gpg.d/
