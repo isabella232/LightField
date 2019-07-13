ed7d76e0a2b4ebcd19abc4322dc51bf1e5a906b8
Ice Karma <icekarma@zive.ca>
2019-07-12 19:14:03 -0700

    Fix version number issue.

--- change-version-number.sh
+++ change-version-number.sh
@@ -38,21 +38,27 @@ cd "${LIGHTFIELD_SRC}"
 
 blue-bar 'Generating src/version.h from src/version.h.in'
 
-sed                                                                    \
-    -e s/@@LIGHTFIELD_VERSION_STRING@@/${VER[0]}.${VER[1]}.${VER[2]}/g \
-    -e  s/@@LIGHTFIELD_VERSION_MAJOR@@/${VER[0]}/g                     \
-    -e  s/@@LIGHTFIELD_VERSION_MINOR@@/${VER[1]}/g                     \
-    -e  s/@@LIGHTFIELD_VERSION_TEENY@@/${VER[2]}/g                     \
-    < "version.h.in"                                                   \
+sed                                                                                                           \
+    -e s/@@LIGHTFIELD_VERSION_STRING@@/${VER[0]}.${VER[1]}.${VER[2]}/g                                        \
+    -e  s/@@LIGHTFIELD_VERSION_MAJOR@@/${VER[0]}/g                                                            \
+    -e  s/@@LIGHTFIELD_VERSION_MINOR@@/${VER[1]}/g                                                            \
+    -e  s/@@LIGHTFIELD_VERSION_TEENY@@/${VER[2]}/g                                                            \
+    < "version.h.in"                                                                                          \
     > "version.h"
 
 cd "${LIGHTFIELD_ROOT}"
 
 blue-bar 'Updating build and packaging scripts'
-perl                                                                                   \
-    -lpi                                                                               \
-    -e "s/^\s*VERSION=\\d+\\.\\d+\\.\\d+\s*$/VERSION=${VER[0]}.${VER[1]}.${VER[2]}/g;" \
-    install-lightfield.sh                                                              \
-    make-deb-package.sh                                                                \
-    make-upgrade-kit.sh                                                                \
+perl                                                                                                          \
+    -lpi                                                                                                      \
+    -e "s/^\s*VERSION=\\d+\\.\\d+\\.\\d+\s*$/VERSION=${VER[0]}.${VER[1]}.${VER[2]}/g;"                        \
+    install-lightfield.sh                                                                                     \
+    make-deb-package.sh                                                                                       \
+    make-upgrade-kit.sh                                                                                       \
     unpack-kit-manually.sh
+
+perl                                                                                                          \
+    -lpi                                                                                                      \
+    -e "s/lightfield-(debug|release)_\\d+\\.\\d+\\.\\d+_amd64/lightfield-$1-${VER[0]}.${VER[1]}.${VER[2]}/g;" \
+    debian/lightfield-debug.postinst                                                                          \
+    debian/lightfield-release.postinst
