d1c0c1b4d7126201156aa73077445dfcf9dc8254
Ice Karma <icekarma@zive.ca>
2019-07-12 18:50:35 -0700

    Or maybe we can't add a fourth component to the version number because older versions of the software will choke.

--- change-version-number.sh
+++ change-version-number.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+function blue-bar () {
+    echo -e "\r\x1B[1;37;44m$*\x1B[K\x1B[0m" 1>&2
+}
+
+function red-bar () {
+    echo -e "\r\x1B[1;33;41m$*\x1B[K\x1B[0m" 1>&2
+}
+
+function error-trap () {
+    red-bar Failed\!
+    exit 1
+}
+
 if [ -z "$1" ]
 then
-    echo 'Usage: change-version-number.sh <newversion>'
+    red-bar 'Usage: change-version-number.sh <newversion>'
     exit 1
 fi
 
@@ -10,14 +23,11 @@ VER=( $(IFS="."; echo $1) )
 
 if [ "${#VER[@]}" -lt 3 ]
 then
-    echo 'Too few components to version number -- must be at least three.'
+    red-bar 'Too few components to version number -- must be at least three.'
     exit 1
-elif [ "${#VER[@]}" -eq 3 ]
+elif [ "${#VER[@]}" -gt 3 ]
 then
-    VER[3]=0
-elif [ "${#VER[@]}" -gt 4 ]
-then
-    echo 'Too many components to version number -- must be at most four.'
+    red-bar 'Too many components to version number -- must be at most three.'
     exit 1
 fi
 
@@ -26,21 +36,23 @@ LIGHTFIELD_SRC="${LIGHTFIELD_ROOT}/src"
 
 cd "${LIGHTFIELD_SRC}"
 
-sed                                                                              \
-    -e s/@@LIGHTFIELD_VERSION_STRING@@/${VER[0]}.${VER[1]}.${VER[2]}.${VER[3]}/g \
-    -e  s/@@LIGHTFIELD_VERSION_MAJOR@@/${VER[0]}/g                               \
-    -e  s/@@LIGHTFIELD_VERSION_MINOR@@/${VER[1]}/g                               \
-    -e  s/@@LIGHTFIELD_VERSION_TEENY@@/${VER[2]}/g                               \
-    -e  s/@@LIGHTFIELD_VERSION_BUILD@@/${VER[3]}/g                               \
-    < "version.h.in"                                                             \
+blue-bar 'Generating src/version.h from src/version.h.in'
+
+sed                                                                    \
+    -e s/@@LIGHTFIELD_VERSION_STRING@@/${VER[0]}.${VER[1]}.${VER[2]}/g \
+    -e  s/@@LIGHTFIELD_VERSION_MAJOR@@/${VER[0]}/g                     \
+    -e  s/@@LIGHTFIELD_VERSION_MINOR@@/${VER[1]}/g                     \
+    -e  s/@@LIGHTFIELD_VERSION_TEENY@@/${VER[2]}/g                     \
+    < "version.h.in"                                                   \
     > "version.h"
 
 cd "${LIGHTFIELD_ROOT}"
 
-perl                                                                                                       \
-    -lpi                                                                                                   \
-    -e "s/^\s*VERSION=\\d+\\.\\d+\\.\\d+(\\.\\d+)?\s*$/VERSION=${VER[0]}.${VER[1]}.${VER[2]}.${VER[3]}/g;" \
-    install-lightfield.sh                                                                                  \
-    make-deb-package.sh                                                                                    \
-    make-upgrade-kit.sh                                                                                    \
+blue-bar 'Updating build and packaging scripts'
+perl                                                                                   \
+    -lpi                                                                               \
+    -e "s/^\s*VERSION=\\d+\\.\\d+\\.\\d+\s*$/VERSION=${VER[0]}.${VER[1]}.${VER[2]}/g;" \
+    install-lightfield.sh                                                              \
+    make-deb-package.sh                                                                \
+    make-upgrade-kit.sh                                                                \
     unpack-kit-manually.sh
