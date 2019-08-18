9509bb421ca191d40a33dbe03be600a609611b64
Ice Karma <icekarma@zive.ca>
2019-08-18 15:47:41 -0700

    Make scrollbar thumbs easier to hit.

--- src/lightfieldstyle.cpp
+++ src/lightfieldstyle.cpp
@@ -15,8 +15,10 @@ LightFieldStyle::~LightFieldStyle( ) {
 }
 
 int LightFieldStyle::pixelMetric( QStyle::PixelMetric metric, QStyleOption const* option, QWidget const* widget ) const {
-    if ( ( QStyle::PM_SliderThickness == metric ) || ( QStyle::PM_SliderLength == metric ) ) {
-        return 50;
+    switch ( metric ) {
+        case QStyle::PM_ScrollBarExtent: return 30;
+        case QStyle::PM_SliderLength:    return 50;
+        case QStyle::PM_SliderThickness: return 50;
+        default:                         return QProxyStyle::pixelMetric( metric, option, widget );
     }
-    return QProxyStyle::pixelMetric( metric, option, widget );;
 }
