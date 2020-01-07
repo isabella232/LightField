77fc30dc2b41a210c936a02d44c9aefe6d547c07
Ice Karma <icekarma@zive.ca>
2020-01-07 14:10:06 -0800

    dlp-4710/set-projector-power: Finishing touches
    * Re-enable code that was disabled because the PRO4710 projector interface didn't like it.
    * Rejigger command-line option handling.

--- dlp-4710/set-projector-power.cpp
+++ dlp-4710/set-projector-power.cpp
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include <fcntl.h>
+#include <getopt.h>
 #include <limits.h>
 #include <signal.h>
 #include <stdint.h>
@@ -9,14 +10,27 @@
 #include <termios.h>
 #include <unistd.h>
 
-enum class Mode {
-    QueryPowerLevel,
-    SetPowerLevel,
-    FirstTimeConfiguration,
-    MonitorPowerAndTemperature,
-};
-
 namespace {
+
+    enum class Mode {
+        QueryPowerLevel,
+        SetPowerLevel,
+        FirstTimeConfiguration,
+    };
+
+    namespace Option {
+        int const Help      = 'h';
+        int const FirstTime = 'f';
+        int const Monitor   = 'm';
+    }
+
+    char const ShortOptions[] { ":h?fm" };
+
+    option const LongOptions[] {
+        { "help",       no_argument, nullptr, Option::Help      },
+        { "first-time", no_argument, nullptr, Option::FirstTime },
+        { "monitor",    no_argument, nullptr, Option::Monitor   },
+    };
 
     bool StringToUnsignedLong( char const* ptr, int const radix, unsigned long* result ) {
         char* endptr = NULL;
@@ -45,7 +59,7 @@ namespace {
         auto len = strlen( buf );
         auto rc  = write( fd, buf, len );
         if ( rc < 0 ) {
-            perror( "WriteCommandToProjector: write" );
+            perror( "set-projector-power: write" );
             return false;
         } else if ( static_cast<size_t>( rc ) < len ) {
             fprintf( stderr, "+ WriteCommandToProjector: short write: %zu expected, %ld written\n", len, rc );
@@ -61,7 +75,7 @@ namespace {
         while ( index < bufferLength ) {
             auto rc = read( fd, &buffer[index], 1 );
             if ( rc < 0 ) {
-                perror( "ReadResponseFromProjector: read" );
+                perror( "set-projector-power: read" );
                 return false;
             } else if ( rc == 0 ) {
                 fprintf( stderr, "+ ReadResponseFromProjector: EOF??\n" );
@@ -99,7 +113,7 @@ namespace {
 
     bool CheckChecksum( unsigned const value, unsigned const checksum ) {
         unsigned tempChecksum = 0;
-        unsigned tempValue = value;
+        unsigned tempValue    = value;
 
         while ( tempValue > 0 ) {
             tempChecksum += tempValue % 10;
@@ -109,68 +123,207 @@ namespace {
         return ( checksum == tempChecksum );
     }
 
+    [[noreturn]]
+    void PrintUsageAndExit( bool success = false ) {
+        fprintf( stderr,
+            "Usage: set-projector-power --first-time\n"
+            "   or: set-projector-power [--monitor] [<value> [<duration>]]\n"
+            "Where:\n"
+            "  -h, -?, --help     display this help and exit\n"
+            "  -f, --first-time   perform first-time projector configuration\n"
+            "  -m, --monitor      monitor projector brightness and temperature\n"
+            "  <value>            brightness, range 0..1023\n"
+            "  <duration>         milliseconds, range 1..65535\n"
+        );
+        exit( success ? EXIT_SUCCESS : EXIT_FAILURE );
+    }
+
+    void MonitorPowerAndTemperature( int const fd ) {
+        unsigned       checksum;
+        unsigned       brightness;
+        unsigned       temperature;
+        siginfo_t      sigInfo;
+        sigset_t       sigset;
+        int            sig;
+        timespec const timeout     { 1, 0 };
+        char           buf[4096]   { };
+
+        sigemptyset( &sigset );
+        sigaddset( &sigset, SIGHUP  );
+        sigaddset( &sigset, SIGINT  );
+        sigaddset( &sigset, SIGQUIT );
+        sigaddset( &sigset, SIGTERM );
+
+        while ( true ) {
+            //
+            // Query LED brightness
+            //
+
+            if ( !SendCommand( fd, buf, "WT+GLGT" ) ) {
+                fprintf( stderr, "\nset-projector-power: command WT+GLGT: failed to send command" );
+                return;
+            }
+            if ( !IsGoodResponse( buf ) ) {
+                fprintf( stderr, "\nset-projector-power: command WT+GLGT: got negative response '%s'\n", buf );
+                return;
+            }
+
+            errno = 0;
+            if ( 2 != sscanf( buf, "OK:%u:%u", &brightness, &checksum ) ) {
+                auto err = errno;
+                if ( err == 0 ) {
+                    fprintf( stderr, "\nset-projector-power: command WT+GLGT: couldn't parse response '%s'\n", buf );
+                } else {
+                    fprintf( stderr, "\nset-projector-power: command WT+GLGT: couldn't parse response '%s': %s [%d]\n", buf, strerror( err ), err );
+                }
+                return;
+            }
+            if ( !CheckChecksum( brightness, checksum ) ) {
+                fprintf( stderr, "\nset-projector-power: command WT+GLGT: got bad checksum\n" );
+                goto next;
+            }
+
+            //
+            // Query LED temperature
+            //
+
+            if ( !SendCommand( fd, buf, "WT+GTMP" ) ) {
+                fprintf( stderr, "\nset-projector-power: command WT+GTMP: failed to send command" );
+                return;
+            }
+            if ( !IsGoodResponse( buf ) ) {
+                fprintf( stderr, "\nset-projector-power: command WT+GTMP: got negative response '%s'\n", buf );
+                return;
+            }
+
+            errno = 0;
+            if ( 2 != sscanf( buf, "OK:%u:%u", &temperature, &checksum ) ) {
+                auto err = errno;
+                if ( err == 0 ) {
+                    fprintf( stderr, "\nset-projector-power: command WT+GTMP: couldn't parse response '%s'\n", buf );
+                } else {
+                    fprintf( stderr, "\nset-projector-power: command WT+GTMP: couldn't parse response '%s': %s [%d]\n", buf, strerror( err ), err );
+                }
+                return;
+            }
+            if ( !CheckChecksum( temperature, checksum ) ) {
+                fprintf( stderr, "\nset-projector-power: command WT+GTMP: got bad checksum\n" );
+                goto next;
+            }
+
+            printf( "\rbrightness: %4u, temperature: %4u °C", brightness, temperature );
+
+        next:
+            sig = sigtimedwait( &sigset, &sigInfo, &timeout );
+            if ( -1 == sig ) {
+                if ( ( EAGAIN == errno ) || ( EINTR == errno ) ) {
+                    continue;
+                } else {
+                    fputc( '\n', stdout );
+                    perror( "set-projector-power: sigtimedwait" );
+                }
+            } else if ( SIGINT == sig ) {
+                fputc( '\n', stdout );
+                exit( 0 );
+            } else {
+                fprintf( stderr, "\n+ sigtimedwait: got unknown signal %d?\n", sig );
+            }
+        }
+    }
+
 }
 
 int main( int argc, char** argv ) {
-    Mode          mode       {   };
-    unsigned long powerLevel {   };
-    //unsigned long duration   { 0 };
+    Mode          mode       { Mode::QueryPowerLevel };
+    unsigned long powerLevel { };
+    unsigned long duration   { 0 };
     int           ret        { 1 };
-    char          buf[4096]  {   };
-    int           fd         {   };
-    termios       term       {   };
+    char          buf[4096]  { };
+    int           fd         { };
+    termios       term       { };
+    bool          monitor    { };
 
     setvbuf( stdout, nullptr, _IONBF, 0 );
 
-    if ( argc > 1 ) {
-        if ( 0 == strcmp( "--first-time", argv[1] ) ) {
-            mode = Mode::FirstTimeConfiguration;
-        } else if ( 0 == strcmp( "--monitor", argv[1] ) ) {
-            mode = Mode::MonitorPowerAndTemperature;
-        } else if ( StringToUnsignedLong( argv[1], 10, &powerLevel ) ) {
-            if ( powerLevel > 1023 ) {
-                fprintf( stderr, "set-projector-power: brightness range is 0..1023\n" );
-                return 1;
-            }
-            mode = Mode::SetPowerLevel;
-        } else {
-            fprintf( stderr,
-                "Usage: set-projector-power [--first-time|--monitor|<value>\n" //" [<duration>]]\n"
-                "Where: --first-time    perform first-time projector configuration\n"
-                "       --monitor       monitor projector brightness and temperature\n"
-                "       <value>         brightness, in range 0..1023\n"
-                //"       <duration>      milliseconds, in range 1..65535\n"
-            );
-            return 1;
+    int opt;
+    do {
+        opt = getopt_long( argc, argv, ShortOptions, LongOptions, nullptr );
+        if ( -1 == opt ) {
+            break;
         }
 
-        //if ( argc > 2 ) {
-        //    if ( StringToUnsignedLong( argv[2], 10, &duration ) ) {
-        //        if ( duration > 65535 ) {
-        //            fprintf( stderr, "set-projector-power: duration range is 0..65535\n" );
-        //            return 1;
-        //        }
-        //    }
-        //}
+        if ( Option::FirstTime == opt ) {
+            if ( monitor ) {
+                fprintf( stderr, "set-projector-power: options --first-time and --monitor may not be combined\n" );
+                PrintUsageAndExit( );
+            }
+            mode = Mode::FirstTimeConfiguration;
+        } else if ( Option::Monitor == opt ) {
+            if ( Mode::FirstTimeConfiguration == mode ) {
+                fprintf( stderr, "set-projector-power: options --first-time and --monitor may not be combined\n" );
+                PrintUsageAndExit( );
+            }
+            monitor = true;
+        } else if ( Option::Help == opt ) {
+            PrintUsageAndExit( true );
+        } else {
+            PrintUsageAndExit( );
+        }
+    } while ( opt != -1 );
+
+    if ( Mode::FirstTimeConfiguration != mode ) {
+        if ( optind < argc ) {
+            if ( StringToUnsignedLong( argv[optind], 10, &powerLevel ) ) {
+                if ( powerLevel > 1023 ) {
+                    fprintf( stderr, "set-projector-power: brightness value %lu out of range\n", powerLevel );
+                    PrintUsageAndExit( );
+                }
+                mode = Mode::SetPowerLevel;
+            }
+            ++optind;
+        }
+        if ( optind < argc ) {
+            if ( StringToUnsignedLong( argv[optind], 10, &duration ) ) {
+                if ( ( 0 == duration ) || ( duration > 65535 ) ) {
+                    fprintf( stderr, "set-projector-power: duration value %lu out of range\n", duration );
+                    PrintUsageAndExit( );
+                }
+            }
+            ++optind;
+        }
     }
 
+    fprintf( stderr,
+        "+ command-line parse result\n"
+        "  + mode:       %d\n"
+        "  + monitor:    %s\n"
+        "  + powerLevel: %lu\n"
+        "  + duration:   %lu\n"
+        "",
+        static_cast<int>( mode ),
+        monitor ? "true" : "false",
+        powerLevel,
+        duration
+    );
+    exit( 69 );
+
     fd = open( "/dev/ttyUSB0", O_RDWR );
-    if ( fd < 0 ) {
-        perror( "open" );
+    if ( -1 == fd ) {
+        perror( "set-projector-power: open" );
         goto bail1;
     }
 
     if ( -1 == tcgetattr( fd, &term ) ) {
-        perror( "tcgetattr" );
+        perror( "set-projector-power: tcgetattr" );
         goto bail2;
     }
 
     if ( -1 == cfsetispeed( &term, B115200 ) ) {
-        perror( "cfsetispeed" );
+        perror( "set-projector-power: cfsetispeed" );
         goto bail2;
     }
     if ( -1 == cfsetospeed( &term, B115200 ) ) {
-        perror( "cfsetospeed" );
+        perror( "set-projector-power: cfsetospeed" );
         goto bail2;
     }
 
@@ -179,7 +332,7 @@ int main( int argc, char** argv ) {
     term.c_cc[VTIME] = 5;
 
     if ( -1 == tcsetattr( fd, TCSANOW, &term ) ) {
-        perror( "tcgetattr" );
+        perror( "set-projector-power: tcsetattr" );
         goto bail2;
     }
 
@@ -222,86 +375,6 @@ int main( int argc, char** argv ) {
             }
             break;
 
-        case Mode::MonitorPowerAndTemperature: {
-            unsigned  checksum;
-            unsigned  brightness;
-            unsigned  temperature;
-            siginfo_t sigInfo;
-            sigset_t  sigset;
-
-            sigemptyset( &sigset );
-            sigaddset( &sigset, SIGINT );
-
-            while ( true ) {
-                if ( !SendCommand( fd, buf, "WT+GLGT" ) ) {
-                    fprintf( stderr, "\nset-projector-power: command WT+GLGT: failed to send command" );
-                    goto bail2;
-                }
-                if ( !IsGoodResponse( buf ) ) {
-                    fprintf( stderr, "\nset-projector-power: command WT+GLGT: got negative response '%s'\n", buf );
-                    goto bail2;
-                }
-
-                errno = 0;
-                if ( 2 != sscanf( buf, "OK:%u:%u", &brightness, &checksum ) ) {
-                    auto err = errno;
-                    if ( err == 0 ) {
-                        fprintf( stderr, "\nset-projector-power: command WT+GLGT: couldn't parse response '%s'\n", buf );
-                    } else {
-                        fprintf( stderr, "\nset-projector-power: command WT+GLGT: couldn't parse response '%s': %s [%d]\n", buf, strerror( err ), err );
-                    }
-                    goto bail2;
-                }
-                if ( !CheckChecksum( brightness, checksum ) ) {
-                    fprintf( stderr, "\nset-projector-power: command WT+GLGT: got bad checksum\n" );
-                    goto next;
-                }
-
-                if ( !SendCommand( fd, buf, "WT+GTMP" ) ) {
-                    fprintf( stderr, "\nset-projector-power: command WT+GTMP: failed to send command" );
-                    goto bail2;
-                }
-                if ( !IsGoodResponse( buf ) ) {
-                    fprintf( stderr, "\nset-projector-power: command WT+GTMP: got negative response '%s'\n", buf );
-                    goto bail2;
-                }
-
-                errno = 0;
-                if ( 2 != sscanf( buf, "OK:%u:%u", &temperature, &checksum ) ) {
-                    auto err = errno;
-                    if ( err == 0 ) {
-                        fprintf( stderr, "\nset-projector-power: command WT+GTMP: couldn't parse response '%s'\n", buf );
-                    } else {
-                        fprintf( stderr, "\nset-projector-power: command WT+GTMP: couldn't parse response '%s': %s [%d]\n", buf, strerror( err ), err );
-                    }
-                    goto bail2;
-                }
-                if ( !CheckChecksum( temperature, checksum ) ) {
-                    fprintf( stderr, "\nset-projector-power: command WT+GTMP: got bad checksum\n" );
-                    goto next;
-                }
-
-                printf( "\rbrightness: %4u, temperature: %4u °C", brightness, temperature );
-
-            next:
-                timespec timeout { 1, 0 };
-                int sig = sigtimedwait( &sigset, &sigInfo, &timeout );
-                if ( -1 == sig ) {
-                    if ( ( EAGAIN == errno ) || ( EINTR == errno ) ) {
-                        continue;
-                    } else {
-                        perror( "\nsigtimedwait" );
-                    }
-                } else if ( SIGINT == sig ) {
-                    fputc( '\n', stdout );
-                    exit( 0 );
-                } else {
-                    fprintf( stderr, "\n+ sigtimedwait: got unknown signal %d?\n", sig );
-                }
-            }
-            break;
-        }
-
         case Mode::SetPowerLevel:
             if ( 0 == powerLevel ) {
                 if ( !SendCommand( fd, buf, "WT+LEDE=%d", 0 ) ) {
@@ -331,27 +404,33 @@ int main( int argc, char** argv ) {
                     goto bail2;
                 }
 
-                //if ( ( duration > 0 ) && !SendCommand( fd, buf, "WT+LEDT=%lu", duration ) ) {
-                //    fprintf( stderr, "set-projector-power: command WT+LEDT: failed to send command" );
-                //    goto bail2;
-                //}
-                //if ( !IsGoodResponse( buf ) ) {
-                //    fprintf( stderr, "set-projector-power: command WT+LEDT: got negative response ('%s')\n", buf );
-                //    goto bail2;
-                //}
+                if ( duration > 0 ) {
+                    if ( !SendCommand( fd, buf, "WT+LEDT=%lu", duration ) ) {
+                        fprintf( stderr, "set-projector-power: command WT+LEDT: failed to send command" );
+                        goto bail2;
+                    }
+                    if ( !IsGoodResponse( buf ) ) {
+                        fprintf( stderr, "set-projector-power: command WT+LEDT: got negative response ('%s')\n", buf );
+                        goto bail2;
+                    }
+                }
             }
             // FALLTHROUGH
 
         case Mode::QueryPowerLevel:
-            //if ( !SendCommand( fd, buf, "WT+LEDR" ) ) {
-            //    fprintf( stderr, "set-projector-power: command WT+LEDR: failed to send command" );
-            //    goto bail2;
-            //}
-            //if ( !IsGoodResponse( buf ) ) {
-            //    fprintf( stderr, "set-projector-power: command WT+LEDR: got negative response ('%s')\n", buf );
-            //    goto bail2;
-            //}
+            if ( !SendCommand( fd, buf, "WT+LEDR" ) ) {
+                fprintf( stderr, "set-projector-power: command WT+LEDR: failed to send command" );
+                goto bail2;
+            }
+            if ( !IsGoodResponse( buf ) ) {
+                fprintf( stderr, "set-projector-power: command WT+LEDR: got negative response ('%s')\n", buf );
+                goto bail2;
+            }
             break;
+    }
+
+    if ( monitor ) {
+        MonitorPowerAndTemperature( fd );
     }
 
     ret = 0;
