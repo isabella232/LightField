b8761d42c387671c11af9e8a2a5bc253135c428e
Ice Karma <icekarma@zive.ca>
2020-03-27 22:01:58 -0700

    Class PrintProfileManager: Adjust API.

--- src/printprofilemanager.cpp
+++ src/printprofilemanager.cpp
@@ -1,25 +1,24 @@
 #include "pch.h"
 
 #include "printprofilemanager.h"
-PrintProfile* PrintProfileManager::_findProfile( QString const& profileName ) {
+
+PrintProfile* PrintProfileManager::getProfile( QString const& profileName ) {
     auto iter = std::find_if( _profiles->begin( ), _profiles->end( ), [&profileName] ( PrintProfile* p ) { return profileName == p->profileName( ); } );
     return ( iter != _profiles->end( ) ) ? *iter : nullptr;
 }
 
 bool PrintProfileManager::addProfile( PrintProfile* newProfile ) {
-    auto profile = _findProfile( newProfile->profileName( ) );
-    if ( !profile ) {
+    if ( auto profile = getProfile( newProfile->profileName( ) ); !profile ) {
         _profiles->append( newProfile );
         return true;
     } else {
-        debug( "PrintProfileManager::addProfile: can't add profile %p: already have a profile named '%s'\n", newProfile, newProfile->profileName( ).toUtf8( ).data( ) );
+        debug( "PrintProfileManager::addProfile: can't add profile: already have one named '%s'\n", newProfile->profileName( ).toUtf8( ).data( ) );
         return false;
     }
 }
 
 bool PrintProfileManager::removeProfile( QString const& name ) {
-    auto profile = _findProfile( name );
-    if ( profile ) {
+    if ( auto profile = getProfile( name ); profile ) {
         _profiles->removeOne( profile );
         return true;
     } else {
@@ -28,14 +27,13 @@ bool PrintProfileManager::removeProfile( QString const& name ) {
     }
 }
 
-bool PrintProfileManager::setActiveProfile( QString const& profileName ) {
-    auto newProfile = _findProfile( profileName );
-    if ( newProfile ) {
-        emit activeProfileChanged( newProfile );
-        _activeProfile = newProfile;
+bool PrintProfileManager::setActiveProfile( QString const& name ) {
+    if ( auto profile = getProfile( name ); profile ) {
+        emit activeProfileChanged( profile );
+        _activeProfile = profile;
         return true;
     } else {
-        debug( "PrintProfileManager::setActiveProfile: couldn't find profile named '%s' in collection\n", profileName.toUtf8( ).data( ) );
+        debug( "PrintProfileManager::setActiveProfile: can't find profile named '%s' in collection\n", name.toUtf8( ).data( ) );
         return false;
     }
 }
--- src/printprofilemanager.h
+++ src/printprofilemanager.h
@@ -19,18 +19,20 @@ public:
         /*empty*/
     }
 
-    PrintProfileCollection const* profiles( ) {
+    PrintProfileCollection const* profiles( ) const {
         return _profiles;
     }
 
-    PrintProfile const* activeProfile( ) {
+    PrintProfile* activeProfile( ) {
         return _activeProfile;
     }
+
+    PrintProfile* getProfile( QString const& name );
 
     bool addProfile( PrintProfile* newProfile );
     bool removeProfile( QString const& name );
 
-    bool setActiveProfile( QString const& profileName );
+    bool setActiveProfile( QString const& name );
 
     bool importProfiles( QString const& fileName );
     bool exportProfiles( QString const& fileName );
@@ -41,8 +43,6 @@ private:
 
     PrintProfileCollection* _profiles      { new PrintProfileCollection };
     PrintProfile*           _activeProfile { };
-
-    PrintProfile* _findProfile( QString const& name );
 
 signals:
     ;
