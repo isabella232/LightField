e7c7b4c103dbc2c99f46dc63391191c46bd5f99f
js <js@conclusive.pl>
2020-04-13 21:49:02 +0200

    Removed redefinition

--- src/constants.h
+++ src/constants.h
@@ -75,9 +75,6 @@ double constexpr          const  NormalFontSize             =   12.0;   // pt
 double constexpr          const  AspectRatio5to3            =    5.0 /  3.0;
 double constexpr          const  AspectRatio16to10          =   16.0 / 10.0;
 
-double constexpr          const  LargeFontSize              =   22.0; // pt
-double constexpr          const  NormalFontSize             =   12.0; // pt
-
 
 
 #   if defined DLP4710
