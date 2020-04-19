fb681c5108b54dd4f2218439dd8ad521dbe8a02a
PiotrConclusive <60405796+PiotrConclusive@users.noreply.github.com>
2020-04-19 22:30:50 +0200

    - improved tiling form activation/deactivation behaviour (#18)
    Co-authored-by: sagittarius89 <piotr.zych@gmx.com>

--- src/tilingtab.h
+++ src/tilingtab.h
@@ -37,6 +37,7 @@ private:
     void _showWarningAndClose ( );
     int  _getMaxCount();
     void _renderText(QPainter* painter, int tileWidth, QPoint pos, double expo);
+    void _setEnabled(bool enabled);
 signals:
     ;
 
@@ -67,11 +68,7 @@ public slots:
                 return;
         }
 
-        this->_confirm->setEnabled( true );
-        this->_step->setEnabled( true );
-        this->_space->setEnabled( true );
-        this->_minExposure->setEnabled( true );
-        this->_count->setEnabled( true );
+        _setEnabled( true );
 
         _showLayerImage();
     }
