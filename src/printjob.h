6635817a8d4aa4dbd65e5a3722546fd3566c28c6
Jakub Klama <jakub@conclusive.pl>
2020-05-09 23:52:34 +0200

    First round of PrintJob cleanup.

--- src/printjob.h
+++ src/printjob.h
@@ -7,59 +7,31 @@
 #include "printprofile.h"
 #include "ordermanifestmanager.h"
 
-class SliceInformation {
-
-public:
-
-
-
-    bool    isPreSliced      { };
-
-    int     layerCount       { };
-    int     layerThickness   { }; // unit: µm
-    int     firstLayerOffset { }; // unit: µm
-
-    int     startLayer       { -1 };
-    int     endLayer         { -1 };
-
-    void setSliceDirectory(const QString &sliceDirectory) {
-        this->_sliceDirectory = sliceDirectory;
-    }
-
-    const QString& sliceDirectory() const {
-        return _sliceDirectory;
-    }
-
-private:
-   QString _sliceDirectory;
+enum class SliceType
+{
+    SliceBase,
+    SliceBody,
 };
 
-class PrintJob {
-
+class SliceInformation
+{
 public:
+    SliceType type;
+    QString sliceDirectory;
+    bool isPreSliced;
+    int layerCount;
+    int layerThickness;
+};
 
-    PrintJob( ) = default;
+class PrintJob
+{
+public:
+    PrintJob() = default;
+    PrintJob(const PrintJob &other) = default;
 
-    PrintJob( PrintJob const* value ) {
-        modelFileName   = value->modelFileName;
-        modelHash       = value->modelHash;
-
-        vertexCount     = value->vertexCount;
-        x               = value->x;
-        y               = value->y;
-        z               = value->z;
-        estimatedVolume = value->estimatedVolume;
-
-        baseSlices      = value->baseSlices;
-        bodySlices      = value->bodySlices;
-        totalLayerCount = value->totalLayerCount;
-
-        printProfile    = value->printProfile;
-    }
-
-    QString                 modelFileName;
-    QString                 modelHash;
-    QString                 currentImageFile        {     };
+    QString modelFileName;
+    QString modelHash;
+    QString currentImageFile;
     
     size_t                  vertexCount     { };
     Coordinate              x               { };
@@ -72,62 +44,79 @@ public:
 
     SliceInformation        baseSlices;
     SliceInformation        bodySlices;
-    int                     firstLayerOffset{ 0 };
-    int                     totalLayerCount { };
 
     PrintProfile*    printProfile    { };
 
-    bool isTiled() {
-        if(_bodyManager) {
+    bool isTiled()
+    {
+        if(_bodyManager)
             return _bodyManager->tiled();
-        }
 
         return false;
     }
 
-    bool isBaseLayer( int const layer ) const {
-        return ( baseSlices.startLayer != -1 ) && ( layer <= baseSlices.endLayer );
+    bool hasBaseLayers() const
+    {
+        return baseSlices.layerCount > 0;
     }
 
-    QString getLayerDirectory( int const layer ) const {
-        if ( isBaseLayer( layer ) ) {
-            return baseSlices.sliceDirectory();
-        } else {
-            return bodySlices.sliceDirectory();
-        }
+    int totalLayerCount() const
+    {
+        return baseSlices.layerCount + bodySlices.layerCount;
     }
 
-    QString getLayerFileName( int const layer ) const {
-        int baseCount = baseSlices.layerCount;
-        int bodyCount = bodySlices.layerCount;
+    int baseThickness() const
+    {
+        return baseSlices.layerCount * baseSlices.layerThickness;
+    }
 
-        if(!_bodyManager.isNull()) {
-            if( layer < baseCount ) {
-                return _baseManager->getElementAt( layer );
-            } else if ( layer - baseCount < bodyCount ) {
-                return _bodyManager->getElementAt( layer - baseCount );
-            }
-        } else {
-            auto sliceDirectory { getLayerDirectory( layer ) };
-            auto adjustedLayer  { layer                      };
+    int baseLayerStart() const
+    {
+        return hasBaseLayers() ? 0 : -1;
+    }
 
-            if ( ( baseSlices.startLayer != -1 ) && !isBaseLayer( layer ) ) {
-                adjustedLayer -= ( baseSlices.endLayer - baseSlices.startLayer + 1 );
-                adjustedLayer += bodySlices.startLayer;
-            }
+    int baseLayerEnd() const
+    {
+        return hasBaseLayers() ? baseSlices.layerCount - 1 : -1;
+    }
 
-            return QString { "%1/%2.png" }.arg( sliceDirectory ).arg( adjustedLayer, 6, 10, DigitZero );
-        }
+    int bodyLayerStart() const
+    {
+        return hasBaseLayers() ? baseSlices.layerCount : 0;
+    }
+
+    int bodyLayerEnd() const
+    {
+        return baseSlices.layerCount + bodySlices.layerCount - 1;
+    }
+
+    bool isBaseLayer(int const layer) const
+    {
+        if (!hasBaseLayers())
+            return false;
+
+        return (layer >= baseLayerStart()) && (layer < baseLayerEnd());
+    }
+
+    QString getLayerDirectory(int const layer) const
+    {
+        return isBaseLayer(layer) ? baseSlices.sliceDirectory :  bodySlices.sliceDirectory;
+    }
+
+    QString getLayerFileName(int const layer) const
+    {
+        return isBaseLayer(layer)
+            ? _baseManager->getElementAt(layer)
+            : _bodyManager->getElementAt(bodyLayerStart() + layer);
     }
 
     QString getLayerPath( int const layer ) const {
-        return getLayerDirectory(layer) % Slash % getLayerFileName(layer);
+        return QString("%1/%2").arg(getLayerDirectory(layer)).arg(getLayerFileName(layer));
     }
 
     double getTimeForElementAt( int position ) {
-        if(_bodyManager && isTiled()) {
+        if(_bodyManager && isTiled())
             return _bodyManager->getTimeForElementAt( position );
-        }
 
         else return -1.0;
     }
@@ -137,44 +126,31 @@ public:
         bodySlices.isPreSliced = true;
         bodySlices.layerCount = _bodyManager->getSize();
 
-        bodySlices.startLayer = _baseManager.isNull() ? 0 : _baseManager->getSize();
-        bodySlices.endLayer = _baseManager.isNull() ? _bodyManager->getSize() : _baseManager->getSize() + _bodyManager->getSize();
-
-
-        if(_bodyManager->tiled()) {
+        if(_bodyManager->tiled())
             bodySlices.layerThickness = _bodyManager->layerThickNessAt(0);
-            bodySlices.firstLayerOffset = _bodyManager->firstLayerOffset();
-        }
 
-        totalLayerCount = bodySlices.layerCount + baseSlices.layerCount;
     }
 
-    void setBaseManager(QSharedPointer<OrderManifestManager> manager) {
-
+    void setBaseManager(QSharedPointer<OrderManifestManager> manager)
+    {
         _baseManager.swap(manager);
 
         if(!_baseManager.isNull()) {
             baseSlices.isPreSliced = true;
             baseSlices.layerCount = _baseManager->getSize();
             baseSlices.layerThickness = _baseManager->layerThickNessAt(0);
-            baseSlices.firstLayerOffset = _baseManager->firstLayerOffset();
-            baseSlices.startLayer = 0;
-            baseSlices.endLayer = _baseManager->getSize() - 1;
         } else {
-            baseSlices.setSliceDirectory( nullptr );
+            baseSlices.sliceDirectory = nullptr;
             baseSlices.isPreSliced = false;
             baseSlices.layerCount = 0;
             baseSlices.layerThickness = -1;
-            baseSlices.firstLayerOffset = 0;
-            baseSlices.startLayer = 0;
-            baseSlices.endLayer = 0;
         }
     }
 
-    int tilingCount() {
-        if(!_bodyManager.isNull() && isTiled()) {
+    int tilingCount()
+    {
+        if(!_bodyManager.isNull() && isTiled())
             return _bodyManager->tilingCount();
-        }
 
         return 0;
     }
