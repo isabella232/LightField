31219de76b20eb89ded7fc1c1b479e948c977670
Pior Zych <piotr@conclusive.pl>
2020-06-24 11:20:51 +0200

    - changed range of exposure time sliders (tiling tab). Allowed 0 sec exposure step.

--- src/tilingtab.h
+++ src/tilingtab.h
@@ -26,9 +26,9 @@ public:
 
 private:
     ParamSlider*            _minExposureBase              { new ParamSlider ("Base layer minimum exposure", "sec", 1, 40, 1, 1, 4 ) };
-    ParamSlider*            _stepBase                     { new ParamSlider ("Base layer exposure step", "sec", 1, 16, 1, 1, 4 )};
+    ParamSlider*            _stepBase                     { new ParamSlider ("Base layer exposure step", "sec", 1, 16, 1, 0, 4 )};
     ParamSlider*            _minExposureBody              { new ParamSlider ("Body layer minimum exposure", "sec", 1, 40, 1, 1, 4 ) };
-    ParamSlider*            _stepBody                     { new ParamSlider ("Body layer exposure step", "sec", 1, 16, 1, 1, 4 )};
+    ParamSlider*            _stepBody                     { new ParamSlider ("Body layer exposure step", "sec", 1, 16, 1, 0, 4 )};
     QPushButton*            _okButton                     { new QPushButton ("Ok" )                };
     QPushButton*            _cancelButton                 { new QPushButton ("Cancel" )            };
 
