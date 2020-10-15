3f827439444f7d017aab6e89bf60ed869b263f16
Pior Zych <piotr@conclusive.pl>
2020-10-15 14:49:02 +0200

    -added debug logs to projectormanager

--- src/projectormanager.h
+++ src/projectormanager.h
@@ -2,6 +2,7 @@
 #define PROJECTORMANAGER_H
 
 #include "../libprojector/src/libprojector.h"
+#include "debug.h"
 
 class ProjectorManager {
 public:
@@ -11,12 +12,15 @@ public:
     }
 
     void turnOnProjector(unsigned int powerLevel) {
+        debug("+ ProjectorManager::turnOnProjector %d\n", powerLevel);
+
         if(portOpen()) {
             pc->setPowerLevel(powerLevel);
         }
     }
 
     void turnOnProjector(unsigned int powerLevel, int duration) {
+        debug("+ ProjectorManager::turnOnProjector %d, duration:  %d ms\n", powerLevel, duration);
         if(portOpen()) {
             pc->setPowerLevel(powerLevel);
             pc->setDuration(duration);
@@ -24,16 +28,22 @@ public:
     }
 
     void turnOffProjector() {
+        debug("+ ProjectorManager::turnOffProjector\n");
+
         if(portOpen()) {
             pc->setPowerLevel(0);
         }
     }
 
     bool isProjectorOn() {
+        debug("+ ProjectorManager::isProjectorOn\n");
+
         return pc->getPowerLevel() > 0;
     }
 private:
     bool portOpen() {
+        debug("+ ProjectorManager::portOpen\n");
+
         if(!isPortOpen) {
             isPortOpen = pc->openPort();
         }
